7f6d45b67da4945ee5d9df2822eeae5d8e60b891
Shropp <69443326+Shropp@users.noreply.github.com>
2022-04-09 22:57:48 -0400

    Update commands.py

--- commands.py
+++ commands.py
@@ -3,20 +3,20 @@ import serial
 import time
 
 OPCODES = {
-    "get health" : b'00',
-    "set health" : b'01',
-    "get skillpoints" : b'10',
-    "set skillpoints" : b'11',
-    "gain item" : b'2',
-    "lose item" : b'3',
-    "get inventory" : b'40',
-    "set inventory" : b'41',
-    "get playername" : b'50',
-    "set playername" : b'51',
-    "get stats" : b'60',
-    "set stats" : b'61',
-    "get exiting_player" : b'7',
-    "send done" : b'8'
+    "get health" : b'\x00\x00',
+    "set health" : b'\x00\x01',
+    "get skillpoints" : b'\x01\x00',
+    "set skillpoints" : b'\x01\x01',
+    "gain item" : b'\x02',
+    "lose item" : b'\x03',
+    "get inventory" : b'\x04\x00',
+    "set inventory" : b'\x04\x01',
+    "get playername" : b'\x05\x00',
+    "set playername" : b'\x05\x01',
+    "get stats" : b'\x06\x00',
+    "set stats" : b'\x06\x01',
+    "get exiting player" : b'\x07',
+    "send done" : b'\x08'
 }
 
 hub = serial.Serial(port='COM13', baudrate=9600, timeout=0.05)
@@ -24,15 +24,20 @@ hub = serial.Serial(port='COM13', baudrate=9600, timeout=0.05)
 def write_commands(player, command, data, reply_expected=False):
     reply = []
 
-    packet = bytes()
-    packet += serial.to_bytes(player)
+    packet = bytearray()
+    packet += bytes((player,))
     packet += OPCODES[command]
 
     if not reply_expected:
-        transformed_data = serial.to_bytes(data)
-        packet += serial.to_bytes(len(transformed_data))
-        packet += serial.to_bytes(transformed_data)
-
+        if type(data) == str:
+            transformed_data = data.encode()
+        elif type(data) == list:
+            transformed_data = serial.to_bytes(data)
+        else:
+            transformed_data = serial.to_bytes((data,))
+        packet += serial.to_bytes((len(transformed_data),))
+        packet += transformed_data
+    
     hub.write(packet)
     time.sleep(0.05)
     if reply_expected:
@@ -40,3 +45,5 @@ def write_commands(player, command, data, reply_expected=False):
 
         reply = hub.read(reply_len)
     return reply
+
+write_commands(0, "set health", 300)
